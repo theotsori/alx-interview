59bb5dcf30d6b7857f2bc0f832732bbcb951c7c8
theotsori <theodoretsori@gmail.com>
2023-07-18 00:24:09 +0300

    Log parsing w/h py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,7 +7,8 @@ import sys
 
 def parse_line(line):
     """
-    <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+    <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+    <status code> <file size>
     """
     try:
         parts = line.split()
@@ -18,39 +19,41 @@ def parse_line(line):
     except (ValueError, IndexError):
         return None, None, None
 
-def print_stats(total_file_size, status_code_counts):
+
+def print_stats(file_size, code_counts):
     """
     function that prints the stdin
     """
-    print(f"Total file size: File size: {total_file_size}")
-    for status_code in sorted(status_code_counts):
-        print(f"{status_code}: {status_code_counts[status_code]}")
+    print(f"File size: {file_size}")
+    for status_code in sorted(code_counts):
+        print(f"{status_code}: {code_counts[status_code]}")
 
 
 def main():
-    total_file_size = 0
-    status_code_counts = {}
+    file_size = 0
+    code_counts = {}
 
     try:
         line_counter = 0
         for line in sys.stdin:
-            ip_address, status_code, file_size = parse_line(line.strip())
+            ip_address, status_code, _size = parse_line(line.strip())
             if ip_address is None:
                 continue
 
-            total_file_size += file_size
-            status_code_counts[status_code] = status_code_counts.get(status_code, 0) + 1
+            file_size += _size
+            code_counts[status_code] = code_counts.get(status_code, 0) + 1
 
             line_counter += 1
             if line_counter % 10 == 0:
-                print_stats(total_file_size, status_code_counts)
+                print_stats(file_size, code_counts)
 
         if line_counter % 10 != 0:
-            print_stats(total_file_size, status_code_counts)
+            print_stats(file_size, code_counts)
 
     except KeyboardInterrupt:
-        print_stats(total_file_size, status_code_counts)
+        print_stats(file_size, code_counts)
         sys.exit(0)
 
+
 if __name__ == '__main__':
-    main() 
+    main()
