c9aecd3d7decf4c9cfb44d335216edb4b9d3a787
theotsori <theodoretsori@gmail.com>
2023-08-28 16:12:19 +0300

    Making Change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,12 +12,14 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    min_coins = [float('inf')] * (total + 1)
-    min_coins[0] = 0
-
-    for i in range(1, total + 1):
-        for coin in coins:
-            if i >= coin:
-                min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
-
-    return min_coins[total] if min_coins[total] != float('inf') else -1
+    else:
+        coin = sorted(coins)
+        coin.reverse()
+        counter = 0
+        for i in coin:
+            while(total >= i):
+                counter += 1
+                total -= i
+        if total == 0:
+            return counter
+        return -1
